3066e4461a9132daf556186bdcf2a39c77626070
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2017-11-05 13:49:14 +0200

    header section

--- src/app/ext.app.js
+++ src/app/ext.app.js
@@ -24,39 +24,67 @@
 					$("#mainNav").addClass("app-sticky");
 				} else {
 					$("#mainNav").removeClass("app-sticky");
-					window.location.hash='home';
+					//window.location.hash='home';
+					//console.log('sticky');
+					changeHash('');
 				}
 
 			}
 			elements.wnd.on("scroll", changeClasses)
 			changeClasses();
 		}
+		
+		var changeHash = function(anchor) {
+			console.log('changing hash to ' + anchor);
+			history.replaceState(null, null, window.location.pathname + anchor);
+		}
 
 		var initMisc = function() {
 			
-			  // Add smooth scrolling on all links inside the navbar
-			  $("#mainNav a").on('click', function(event) {
-			    // Make sure this.hash has a value before overriding default behavior
-			    if (this.hash !== "") {
-			      // Prevent default anchor click behavior
-			      event.preventDefault();
-
-			      // Store hash
-			      var hash = this.hash;
-
-			      // Using jQuery's animate() method to add smooth page scroll
-			      // The optional number (800) specifies the number of milliseconds it
-						// takes to scroll to the specified area
-			      $('html, body').animate({
-			        scrollTop: $(hash).offset().top
-			      }, 800, function(){
-			   
-			        // Add hash (#) to URL when done scrolling (default click
-							// behavior)
-			        window.location.hash = hash;
-			      });
-			    }  // End if
-			  });
+		  // Smooth scrolling using jQuery easing
+		  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(event) {
+		    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		      event.preventDefault();
+		    	var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		      if (target.length) {
+		        $('html, body').animate({
+		          scrollTop: (target.offset().top - 56)
+		        }, 600, "easeInOutExpo");
+		        //console.log('a click');
+		        changeHash(this.hash);
+		        return false;
+		      }
+		    }
+		  });
+			
+			
+			
+			
+			
+//			  // Add smooth scrolling on all links inside the navbar
+//			  $("#mainNav a").on('click', function(event) {
+//			    // Make sure this.hash has a value before overriding default behavior
+//			    if (this.hash !== "") {
+//			      // Prevent default anchor click behavior
+//			      event.preventDefault();
+//
+//			      // Store hash
+//			      var hash = this.hash;
+//
+//			      // Using jQuery's animate() method to add smooth page scroll
+//			      // The optional number (800) specifies the number of milliseconds it
+//						// takes to scroll to the specified area
+//			      $('html, body').animate({
+//			        scrollTop: ($(hash).offset().top - 56)
+//			      }, 600, "easeInOutExpo", function(){
+//			   
+//			        // Add hash (#) to URL when done scrolling (default click
+//							// behavior)
+//			        window.location.hash = hash;
+//			      });
+//			    }  // End if
+//			  });
 			
 			// Closes responsive menu when a scroll trigger link is clicked
 			$('.js-scroll-trigger').click(function() {
@@ -74,12 +102,15 @@
 			});
 			
 		  $(window).on('activate.bs.scrollspy', evt => {
+		  	//TODO use observable to reduce the changes
 		    var l = $("#mainNav a.active");
 		    if(l) {
 		    	if (l.length)
 		    		l = l[0];
-		    	if (l && l.hash)
-		    		window.location.hash = l.hash;
+		    	if (l && l.hash) {
+		    		//console.log('scrollspy');
+		    		changeHash(l.hash); //window.location.hash = l.hash;
+		    	}
 		    }
 		  });
 		  
@@ -316,7 +347,7 @@
 				// googleMap()
 				// isotopePortfolio()
 				// navigation()
-				// wow()
+				wow();
 				// countdown()
 				// placeholderIE()
 				// validateAndSend()
